43f844dcc60a5759177c3ee268952ca944db9769
Samuel-Tefera <samuelteferab@gmail.com>
2024-07-17 14:55:09 +0300

    automate a current year in copyright text

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
-console.log("Hello world!");
-
-const myName = "Jonas Schmedtmann";
-const h1 = document.querySelector(".heading-primary");
-console.log(myName);
-console.log(h1);
+// Set current year
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
 
 ///////////////////////////////////////////////////////////
 // Smooth scrolling animation
