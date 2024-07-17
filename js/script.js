0faac747903ca74b549f7410e8400f128f06f110
Samuel-Tefera <samuelteferab@gmail.com>
2024-07-17 14:55:06 +0300

    give a smooth scrolling animation for safari browser

--- js/script.js
+++ js/script.js
@@ -3,6 +3,35 @@ const yearEl = document.querySelector(".year");
 const currentYear = new Data().getFullYear();
 yearEl.textContent = currentYear;
 
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
